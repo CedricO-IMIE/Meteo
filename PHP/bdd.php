7bc5e47f1e792a62de5cf2848e9fdb1d19000b04
CedricO-IMIE <cedric.olivry@imie.fr>
2019-02-20 11:25:57 +0100

    Base de données

--- PHP/bdd.php
+++ PHP/bdd.php
@@ -1,13 +1,23 @@
-<?php 
-// On met en variables les informations de connexion 
-$hote = 'localhost'; // Adresse du serveur 
-$login = 'root'; // Login 
-$pass = ''; // Mot de passe 
-$base = 'jsdigi'; // Base de données à utiliser 
- 
-// On se connecte à la base de données 
-mysql_connect($hote, $login, $pass); 
- 
-// On selectionne la base de données souhaitée 
-mysql_select_db($base); 
+<?php
+
+
+$conn = new msqli($host, $login, $pass, $db);
+
+
+
+function insertOrUpdateMeteo($lieu, $temprt, $lat, $lon, $humidite,
+								 $pression, $ventv, $ventd, $temps) {
+
+		$conn->mysqli_query("select * id from meteo where lieu like".$lieu.";");
+
+		if ( mysqli_num_rows($result) > 0 ) { // update
+				$sql = "update meteo set temp = ".$temprt.", humidite = ". $humidite .
+					", pression = ". $pression .
+					"where lieu like ".$lieu.";";
+		}
+		else { // insert
+				$insert = "INSERT INTO meteo (Lieu,Temperature,Lat,Lon,Humidite,Pression,Temps,VentV,VentDir) VALUES ('Nantes',10,12,12,12,10,'bo',10,10)";
+		}
+}
+
 ?>
