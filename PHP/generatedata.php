794b03e9f55faedef7ed7bb4aea43b4b0adc72d4
CedricO-IMIE <cedric.olivry@imie.fr>
2019-02-20 14:36:09 +0100

    Base de données

--- PHP/generatedata.php
+++ PHP/generatedata.php
@@ -6,7 +6,7 @@ session_start();
 
 //$sql = "INSERT INTO valeurs (temperature,lieu,latitude,longitude,humidite,pression,temps,vitesse_vent,direction_vent) VALUES (20,'nantes',12,12,25,18,'beau',120,1)";
 
-function insertOrUpdateMeteo($lieu, $temperature, $lat, $lon, $humidite, $pression, $ventv, $ventdir, $temps) {
+function insertOrUpdateMeteo($lieu, $temperature, $lat, $lon, $humidite, $pression, $temps, $ventv, $ventdir) {
 
         $database = mysqli_connect('localhost', 'root', '', 'jsdigi');
 
@@ -16,28 +16,28 @@ function insertOrUpdateMeteo($lieu, $temperature, $lat, $lon, $humidite, $pressi
         echo "Connecté à la base de données. <br>";
     }
 
-    $result = mysqli_query($database, "SELECT * FROM meteo WHERE Lieu like '".$lieu."'");
+    $query = mysqli_query($database, "SELECT * FROM meteo WHERE Lieu like '".$lieu."'");
 
-    if (!$result) {
+    if (!$query) {
         die('Error: ' . mysqli_error($database));
     }
 
-    if (mysqli_num_rows($result) > 0) {
+    if (mysqli_num_rows($query) > 0) {
 
         echo "La ville existe déjà !";
-        $sql = "UPDATE meteo SET Temperature='".$temperature."', Lat ='".$lat."' , Lon ='".$lon."', Humidite='".$humidite."', Pression='".$pression."' , Temps='".$temps."' , VentV='".$ventv.
-        "', VentDir= '".$ventdir."'
+        $sql = "UPDATE meteo SET Temperature='".$temperature."' , Lat ='".$lat."' , Lon ='".$lon."' , Humidite='".$humidite."' , Pression='".$pression."' , Temps='".$temps."' , VentV='".$ventv.
+        "' , VentDir= '".$ventdir."'
         WHERE lieu like'".$lieu."';";
         var_dump($sql);
-        //mysqli_query($conn,$sql);
+        $database->query($sql);
     } else {
 
         echo "La ville à été créee";
         $sql = "INSERT INTO meteo (Lieu,Temperature,Lat,Lon,Humidite,Pression,Temps,VentV,VentDir) VALUES ($lieu,$temperature,$lat,$lon,$humidite,$pression,$temps,$ventv,$ventdir)";
         var_dump($sql);
-        //mysqli_query($conn,$sql);
+        mysqli_query($database,$sql);
     }
-    mysqli_query($conn,$sql);
+    
 }
 
 insertOrUpdateMeteo('lemans',45,42,43,48,56,'beau',124,1);
