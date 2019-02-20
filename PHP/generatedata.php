1828eaf26c2c520bb865dc8c87dbf516f27e33a5
CedricO-IMIE <cedric.olivry@imie.fr>
2019-02-20 13:44:50 +0100

    Base de données

--- PHP/generatedata.php
+++ PHP/generatedata.php
@@ -3,7 +3,7 @@ session_start();
 
 
 
-// Insertion des variables
+
 //$sql = "INSERT INTO valeurs (temperature,lieu,latitude,longitude,humidite,pression,temps,vitesse_vent,direction_vent) VALUES (20,'nantes',12,12,25,18,'beau',120,1)";
 
 function insertOrUpdateMeteo($lieu, $temperature, $lat, $lon, $humidite, $pression, $ventv, $ventdir, $temps) {
@@ -16,29 +16,31 @@ function insertOrUpdateMeteo($lieu, $temperature, $lat, $lon, $humidite, $pressi
         echo "Connecté à la base de données. <br>";
     }
 
-    $query = mysqli_query($database, "SELECT * FROM meteo WHERE Lieu like '".$lieu."'");
+    $result = mysqli_query($database, "SELECT * FROM meteo WHERE Lieu like '".$lieu."'");
 
-    if (!$query) {
+    if (!$result) {
         die('Error: ' . mysqli_error($database));
     }
 
-    if (mysqli_num_rows($query) > 0) {
+    if (mysqli_num_rows($result) > 0) {
 
         echo "La ville existe déjà !";
         $sql = "UPDATE meteo SET Temperature='".$temperature."', Lat ='".$lat."' , Lon ='".$lon."', Humidite='".$humidite."', Pression='".$pression."' , Temps='".$temps."' , VentV='".$ventv.
         "', VentDir= '".$ventdir."'
         WHERE lieu like'".$lieu."';";
-        $database->query($sql);
+        var_dump($sql);
+        //mysqli_query($conn,$sql);
     } else {
 
         echo "La ville à été créee";
         $sql = "INSERT INTO meteo (Lieu,Temperature,Lat,Lon,Humidite,Pression,Temps,VentV,VentDir) VALUES ($lieu,$temperature,$lat,$lon,$humidite,$pression,$temps,$ventv,$ventdir)";
-        $database->query($sql);
+        var_dump($sql);
+        //mysqli_query($conn,$sql);
     }
-
+    mysqli_query($conn,$sql);
 }
 
-insertOrUpdateMeteo('nantes',45,42,43,48,56,'beau',124,1);
+insertOrUpdateMeteo('lemans',45,42,43,48,56,'beau',124,1);
 
   /*  if ($database->query($sql) === true) {
         echo "Query executed without issue.";
