fce3557239756ccdbd9aa0f9dcefe253cea34ba2
CedricO-IMIE <cedric.olivry@imie.fr>
2019-02-22 12:27:55 +0100

    PHP

--- HTML/index.php
+++ HTML/index.php
@@ -1,5 +1,118 @@
-<?php 
-include 'bdd.php';
+<!DOCTYPE html>
+<html>
 
 
-?>
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="leaflet.css" />
+		<link rel="stylesheet" href="CSS/style.css" />
+		<script src="leaflet.js"></script>
+		<script src="https://code.jquery.com/jquery-3.3.1.js" integrity="sha256-2Kok7MbOyxpgUVvAk/HJ2jigOSYS2auK4Pfzbm7uH60=" crossorigin="anonymous"></script>
+	</head>
+
+
+	<body>
+		<div id="maCarte" style="height: 700px ; width:1400px">
+		</div>
+		 
+		<div class="bloc">
+			<div id="temps">
+				<span> Temps </span> <br>
+				<span id="temp"> Neigeux </span>
+			</div>
+			 
+			<hr>
+			 
+			<div id="temperature">
+				<span> Temperature </span> <br>
+				<span id="degre"> 19 </span> °C
+			</div>
+		 
+			<hr>
+		 
+			<div id="humidite">
+				<span> Humidite </span> <br>
+				<span id="humi"> 0 </span> %
+			</div>
+		 
+			<hr>
+
+			<div id="direction">
+				<span> Direction vent </span> <br>
+				<span id="dir"> Vers içi </span>
+			</div>
+		 
+		</div>
+
+		<script>
+
+			function main() {
+
+			var maCarte = L.map('maCarte').setView([46.5, 2], 6.1);
+			 
+			L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+			maxZoom: 19,
+			attribution: ' <a href="http://www.openstreetmap.org/copyright%22%3EOpenStreetMap</a>'
+			}).addTo(maCarte);
+
+			maCarte.on('click', 
+                    function(e){
+                        var coord = e.latlng.toString().split(',');
+                        var lat = coord[0].split('(');
+                        var lng = coord[1].split(')');
+                        console.log("Latitude: " + lat[1] + " Longitude:" + lng[0]);
+                    });
+            }
+                    window.onload = main;
+			 
+			function changetemps(t) {
+				$('#temp').text(t);
+			};
+			 
+			function changetemperature(te) {
+				$('#degre').text(te);
+			};
+			 
+			function changehumi(h) {
+				$('#humi').text(h);
+			};
+			 
+			function changedir(d) {
+				$('#dir').text(d);
+			};
+			 
+			$(function appel() {
+				changetemps('yo');
+				changetemperature(50);
+				changehumi(20);
+				changedir('Sud/Sud-Est');
+			});
+			
+
+			var meteovar = {
+				Temperature: 30,
+				Temps: 'Beau',
+				Humidite: 50,
+				Direction: 'Nord',
+				Pression: 1050,
+				Vitesse: 5,
+				Lieu: 'Nantes'
+			};
+
+			function ChangeMeteo(jsonMeteo){
+			    changetemps(jsonMeteo.Temps);
+			    changetemperature(jsonMeteo.Temperature);
+			    changehumi(jsonMeteo.Humidite);
+			    changedir(jsonMeteo.vent_dir);
+			}
+
+			$(function () {
+				ChangeMeteo(meteovar);
+				ChangeMeteo(42);
+			});
+ 
+		 </script>
+		</body>
+
+
+</html>
--- PHP/generatedata.php
+++ PHP/generatedata.php
@@ -1,56 +1,52 @@
 <?php
-session_start();
+if (!function_exists("mysqli_connect"))
+    die("Le support MySql n'est pas disponible.");
+if (!@mysqli_connect("localhost", "root", "", "jsdigi"))
+    die('Base de donnée - mysql_connect: ' . mysqli_error());
+ 
+$conn = mysqli_connect("localhost", "root", "", "jsdigi"); // new mysqli($host, $login, $pass, $db);
+ 
+function insertOrUpdate($conn,$temprt,$lieu,$lat,$lon,$Humidite,$Pression,$Temps,$ventv,$ventd){
+
+$tableau = array( "lat" => 42, "lon" => -1, "Lieu" => "Nantes");
+$json = json_encode($tableau);
+
+function getInfosMeteo($lat,$lon) 
+//TODO : modifier le where de la requet pour filtrer par lat/lon 20km près
+ 
+    $result = mysqli_query($conn,"select * from meteo where Lieu like '$lieu';");
+    var_dump(mysqli_error($conn));
+ 
+if ( mysqli_num_rows($result) > 0){
+//recupérer les données de la requete plus haut
+//Construire un Json avec
+$sql = "update meteo set Temperature = $temprt , Humidite = $Humidite , Pression =  $Pression
+                 where Lieu like '$lieu' ;";
+                 var_dump($sql);
+                //echo($sql);
+} else { //insert
 
 
-
-
-//$sql = "INSERT INTO valeurs (temperature,lieu,latitude,longitude,humidite,pression,temps,vitesse_vent,direction_vent) VALUES (20,'nantes',12,12,25,18,'beau',120,1)";
-
-function insertOrUpdateMeteo($lieu, $temperature, $lat, $lon, $humidite, $pression, $temps, $ventv, $ventdir) {
-
-        $database = mysqli_connect('localhost', 'root', '', 'jsdigi');
-
-    if (!$database) {
-        die("Connection échouée: " . mysqli_connect_error());
-    } else {
-        echo "Connecté à la base de données. <br>";
-    }
-
-    $query = mysqli_query($database, "SELECT * FROM meteo WHERE Lieu like '".$lieu."'");
-
-    if (!$query) {
-        die('Error: ' . mysqli_error($database));
-    }
-
-    if (mysqli_num_rows($query) > 0) {
-
-        echo "La ville existe déjà !";
-        $sql = "UPDATE meteo SET Temperature='".$temperature."' , Lat ='".$lat."' , Lon ='".$lon."' , Humidite='".$humidite."' , Pression='".$pression."' , Temps='".$temps."' , VentV='".$ventv.
-        "' , VentDir= '".$ventdir."'
-        WHERE lieu like'".$lieu."';";
+ include 'API.php'
+//reutiliser le code ci dessous pour stocker les info en base
+//executer la requete
+//construire un json avec
+        $sql = "INSERT INTO meteo (Lieu, Temperature, Lat, Lon, Humidite, Pression, Temps, VentV, VentDir) values ('$lieu', $temprt, $lat, $lon, $Humidite, $Pression,'$Temps', $ventv, $ventd);";
         var_dump($sql);
-        $database->query($sql);
-    } else {
-
-        echo "La ville à été créee";
-        $sql = "INSERT INTO meteo (Lieu,Temperature,Lat,Lon,Humidite,Pression,Temps,VentV,VentDir) VALUES ($lieu,$temperature,$lat,$lon,$humidite,$pression,$temps,$ventv,$ventdir)";
-        var_dump($sql);
-        mysqli_query($database,$sql);
+        //echo($sql);
     }
-    
+mysqli_query($conn,$sql);
+var_dump("erreur:".mysqli_error($conn));
+//TODE : Renvoyer le JSON
+//return $json;
 }
 
-insertOrUpdateMeteo('lemans',45,42,43,48,56,'beau',124,1);
+ 
+insertOrUpdate($conn,30, 'nantes',45,2,43,1020,'beau',12,1);
 
-  /*  if ($database->query($sql) === true) {
-        echo "Query executed without issue.";
-        //header('location:index.php');
-    } else {
-        echo "Erreur: " . $sql . "<br>" . $database->error;
-    } */
+   
 
-
-Session_destroy();
+$conn->close();
 
 
 ?>
